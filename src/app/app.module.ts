219be398331bc56166072bbcdd34d1d55a17b1c0
mrboggii <pruteanbogdan@gmail.com>
2020-06-06 19:56:28 +0200

    front almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,64 @@
+// Modules
+import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing, appRoutingProviders } from './app.routing';
+import { MomentModule } from 'angular2-moment';
 
+import { HttpModule } from '@angular/http';
+
+// Module Custom
+import { MessagesModule} from './messages/messages.module';
+
+// Components
 import { AppComponent } from './app.component';
-import { FollowedComponent } from './followed/followed.component';
-import { FollowingComponent } from './following/following.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { PublicationsComponent } from './publications/publications.component';
-import { RegisterComponent } from './register/register.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TimelineComponent } from './timeline/timeline.component';
-import { UserEditComponent } from './user-edit/user-edit.component';
-import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FollowingComponent } from './components/following/following.component';
+import { FollowedComponent } from './components/followed/followed.component';
+
+// Services
+import {UserService} from './services/user.service';
+import {UserGuard} from './services/user.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FollowedComponent,
-    FollowingComponent,
-    HomeComponent,
     LoginComponent,
-    ProfileComponent,
-    PublicationsComponent,
     RegisterComponent,
+    HomeComponent,
+    UserEditComponent,
+    UsersComponent,
     SidebarComponent,
     TimelineComponent,
-    UserEditComponent,
-    UsersComponent
+    PublicationsComponent,
+    ProfileComponent,
+    FollowingComponent,
+    FollowedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routing,
+    HttpClientModule,
+    MomentModule,
+    MessagesModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+      appRoutingProviders,
+      UserService,
+      UserGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
