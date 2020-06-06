c90321bf0fa415c4bd55d66b12a15099dc05a118
mrboggii <pruteanbogdan@gmail.com>
2020-06-06 18:58:52 +0200

    followed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FollowedComponent } from './followed/followed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FollowedComponent
   ],
   imports: [
     BrowserModule
